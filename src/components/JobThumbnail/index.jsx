27d2fc4bafb128309f78b73d49e23ecb31471251
brunoannunciato <brunobannunciato@gmail.com>
2019-07-30 11:14:53 -0300

    change identation of spaces to tabs

--- src/components/JobThumbnail/index.jsx
+++ src/components/JobThumbnail/index.jsx
@@ -3,23 +3,20 @@ import React, { Component } from 'react';
 import './style.scss';
 
 export default class JobThumbnail extends Component  {
-    componentDidMount = () => {
-        console.log(this.props.link); 
-    }
 
-    render () {
-        return (
-            <div className="thumbnail" onClick={this.props.handleClick}>
-                <a href={this.props.link !== undefined ? this.props.link + '?utm_campaign=brunodev' : 'javascript:void(0)'} target='_blank' rel="noopener noreferrer">
-                    <figure>
-                        <img src={this.props.image} alt=""/>
-                    </figure>
-                </a>
-                <p className='thumbnail__title'><strong>Project: </strong>{this.props.name}</p>
-                <div className="thumbnail__technologies">
-                    <p className="languages"><strong>technologies: </strong>{this.props.languages}</p>
-                </div>
-            </div>
-        )
-    }
+	render () {
+		return (
+			<div className="thumbnail" onClick={this.props.handleClick}>
+				<a href={this.props.link !== undefined ? this.props.link + '?utm_campaign=brunodev' : 'javascript:void(0)'} target='_blank' rel="noopener noreferrer">
+					<figure>
+						<img src={this.props.image} alt=""/>
+					</figure>
+				</a>
+				<p className='thumbnail__title'><strong>Project: </strong>{this.props.name}</p>
+				<div className="thumbnail__technologies">
+					<p className="languages"><strong>technologies: </strong>{this.props.languages}</p>
+				</div>
+			</div>
+		)
+	}
 }
